9a31448eda50b43294997a52e63d5cb6ec1a13a4
guigeek123 <guillaume.coindet@gmail.com>
2018-08-30 18:25:09 +0200

    Reorganise and adding sonar configuration scripts (for findsecbugs)

--- bootstrap-infra/access-scripts/access_jenkins.sh
+++ bootstrap-infra/access-scripts/access_jenkins.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-printf "Your password for jenkins access is : "
+printf "\nYour password for jenkins access is : "
 printf $(kubectl get secret cd-jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode);echo
 
-printf "\n Setting up port forwarding for Jenkins ..."
+printf "Setting up port forwarding for Jenkins ..."
 
 export POD_NAME=$(kubectl get pods -l "component=cd-jenkins-master" -o jsonpath="{.items[0].metadata.name}")
 kubectl port-forward $POD_NAME 8080:8080 >> /dev/null &
