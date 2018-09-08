f8056f7538899d9977e1ad1006c536cd3f66adcb
guigeek123 <guillaume.coindet@gmail.com>
2018-09-08 17:59:58 +0200

    Script jobs creation

---
+++ bootstrap-infra/demo_jobs_setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+display_usage_and_exit() {
+  echo "Usage: $(basename "$0") <jenkins token api>" >&2
+  echo "Arguments:" >&2
+  echo "Jenkins Token API REQUIRED: name of the job to create" >&2
+  exit 1
+}
+
+shift $((OPTIND-1))
+if [ "$#" -ne 1 ] ; then
+  display_usage_and_exit
+fi
+readonly token="$1"
+
+app1=( "bla1" "https://github.com/guigeek123/spring-petclinic-jenkins-kubernetes.git" "cleaning" )
+app2=( "bla2" "https://github.com/guigeek123/spring-petclinic-jenkins-kubernetes.git" "cleaning" )
+
+scripts/createJob.sh -k $token -b ${app1[2]} -g ${app1[1]} ${app1[0]}
+#scripts/createJob.sh -k $token -b ${app2[2]} -g ${app2[1]} ${app2[0]}
---
+++ bootstrap-infra/scripts/createJob.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+DEFAULT_JENKINS_URL=http://localhost:8080/
+DEFAULT_JENKINS_USER=admin
+DEFAULT_BRANCH=master
+TEMPLATE_PATH=seedjobs/templateJobConfig.xml
+
+display_usage_and_exit() {
+  echo "Usage: $(basename "$0") [-k <jenkins token>] [-f <XML job description relative file path>] <jobName>" >&2
+  echo "Arguments:" >&2
+  echo "jobName REQUIRED: name of the job to create" >&2
+  echo "-k REQUIRED: Jenkins token api for admin user (default) or user specified with '-u' option" >&2
+  echo "-b REQUIRED: Github URL of project" >&2
+  echo "-u OPTIONAL: Jenkins user, default is 'admin" >&2
+  echo "-n OPTIONAL: Jenkins URL with port, default is 'http://localhost:8080/" >&2
+  echo "-b OPTIONAL: Git branch name configured in the job within, default 'master'" >&2
+  exit 1
+}
+
+configure_template() {
+  cp ${path} ${path}.bak
+  sed -i "s#GITHUBURL#${githubprojecturl}#" ${path}
+  sed -i "s#BRANCHNAME#${branch}#" ${path}
+}
+
+
+create_job() {
+  CRUMB=$(curl -s ${url}'crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' -u $user:$token)
+  curl -s -XPOST "${url}createItem?name=${jobname}" -u ${user}:${token} --data-binary @${path} -H "$CRUMB" -H "Content-Type:text/xml"
+}
+
+clean() {
+  rm ${path}
+  mv ${path}.bak ${path}
+}
+
+start_job() {
+  CRUMB=$(curl -s ${url}'crumbIssuer/api/xml?xpath=concat(//crumbRequestField,":",//crumb)' -u $user:$token)
+  curl -X POST "${url}job/${jobname}/build" -H "$CRUMB" -u ${user}:${token}
+}
+
+
+url=${DEFAULT_JENKINS_URL}
+user=${DEFAULT_JENKINS_USER}
+branch=${DEFAULT_BRANCH}
+path=${TEMPLATE_PATH}
+
+while getopts ':k:u:n:b:g:' arg
+do
+    case ${arg} in
+        n) url=${OPTARG};;
+        t) user=${OPTARG};;
+        k) token=${OPTARG};;
+        b) branch=${OPTARG};;
+        g) githubprojecturl=${OPTARG};;
+        *) display_usage_and_exit
+    esac
+done
+
+
+shift $((OPTIND-1))
+if [ "$#" -ne 1 ] ; then
+  display_usage_and_exit
+fi
+readonly jobname="$1"
+
+if [ -z "$token" ] ; then
+  display_usage_and_exit
+fi
+
+if [ -z "$githubprojecturl" ] ; then
+  display_usage_and_exit
+fi
+
+configure_template
+create_job
+clean
+start_job
